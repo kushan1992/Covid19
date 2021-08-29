f368eaa2a69920169300594499c591f3f1e3017b
kushan <kushan.pabasara78@gmail.com>
2021-08-29 16:46:05 +0530

    main: fixed store middleware

--- src/store.js
+++ src/store.js
@@ -2,11 +2,19 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+const devtools =
+  process.env.NODE_ENV === "test"
+    ? (x) => x /* eslint-disable no-underscore-dangle */
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__();
+/* eslint-enable no-underscore-dangle */
+
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devtools
   )
 );
 
