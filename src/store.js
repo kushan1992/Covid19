07d573061ee61c5a884e7a3a3c603e1091758170
kushan <kushan.pabasara78@gmail.com>
2021-08-29 17:14:34 +0530

    main: fixed isse of deploy

--- src/store.js
+++ src/store.js
@@ -2,18 +2,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const devtools =
-  process.env.NODE_ENV === "test"
-    ? (x) => x /* eslint-disable no-underscore-dangle */
-    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__();
-/* eslint-enable no-underscore-dangle */
-
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (f) => f
